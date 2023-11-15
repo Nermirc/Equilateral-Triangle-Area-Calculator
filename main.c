3e4a90826013b5298e2481e236d1c673cdc5c312
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-11-15 10:29:42 +0300

    Equilateral Triangle Area Calculator
    This program calculates area of equilateral triangle

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+    float ucgeninalani(int A,int B,int C)
+    {
+        float u,alan;
+        u=(A+B+C)/2;
+        alan=(sqrt(u*(u-A)*(u-B)*(u-C)));
+        return alan;
+    }
+int main()
+{
+    //Equilateral Triangle Area Calculator//
+    int a,b,c;
+    printf("Uc kenar uzunlugunu giriniz.\n");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("Eskenar ucgenin alani =%.2f",ucgeninalani(a,b,c));
+}
